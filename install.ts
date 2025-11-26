dcaa3063025c2e5daac58b142d8920ea7ac2af2a
robinhawiz <Robin.Hawiz.6670@student.uu.se>
2025-11-26 20:05:54 +0100

    feat(db): add SQLite database and install script

---
+++ install.ts
@@ -0,0 +1,32 @@
+// Load environment variables
+import "./config/env";
+
+import DatabaseConstructor, { type Database } from "better-sqlite3";
+
+let db: Database | undefined;
+
+try {
+  // Connect to db
+  const dbPath = process.env.DATABASE;
+  if (!dbPath) {
+    throw new Error("Failed to create db: Missing DATABASE in .env");
+  }
+  db = new DatabaseConstructor(dbPath);
+
+  // Create table book
+  db.exec("drop table if exists book;");
+
+  db.exec(`CREATE TABLE book(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        published_year INTEGER NOT NULL,
+        is_read BOOLEAN NOT NULL
+        );`);
+
+  console.log("DB initialized at:", dbPath);
+} catch (e) {
+  console.error("---ERROR---");
+  console.error(e instanceof Error ? e.message : e);
+} finally {
+  if (db) db.close();
+}
